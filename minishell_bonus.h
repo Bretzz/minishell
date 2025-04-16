25e9b60fc45d6178df9d499d282d0a54d1bc482d
Tommaso Piana Agostinetti <topiana-@c2r3p9.42firenze.it>
2025-04-16 17:09:06 +0200

    normi bonus header

--- minishell_bonus.h
+++ minishell_bonus.h
@@ -13,7 +13,7 @@
 #ifndef MINISHELL_BONUS_H
 # define MINISHELL_BONUS_H
 
-#include "minishell.h"
+# include "minishell.h"
 
 typedef enum e_logig
 {
@@ -38,8 +38,10 @@ int		syntax_line_bonus(char **line, char ***vars);
 t_cmd	*parse_tokens_bonus(char *line, const char ***vars);
 void	bongou_stray_docs_bonus(char *line, const char ***vars);
 
-int		execute_command_bonus(char *line, t_bush *bush, t_cmd *cmd, char ***vars);
-int		execute_pipeline_bonus(char *line, t_bush *bush, t_cmd *cmd, char ***vars);
+int		execute_command_bonus(char *line,
+			t_bush *bush, t_cmd *cmd, char ***vars);
+int		execute_pipeline_bonus(char *line,
+			t_bush *bush, t_cmd *cmd, char ***vars);
 
 char	*expand_string_bonus(char *str, const char ***vars);
 // char	*just_expand_string_bonus(char *str, const char ***vars);
